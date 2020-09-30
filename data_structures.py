d2e672d237fa4ae7141ef8d9e57dfd12a7df6d27
Paras Pandya <paras.4eva@gmail.com>
2020-09-30 14:54:27 +0530

    Data structure: Queue - Added decorator to handle multible data structure ADT

--- data_structures.py
+++ data_structures.py
@@ -18,6 +18,41 @@ class DataStructure():
     """
     Class to demonstrate data structures along with ADTs
     """
+    # Available data structure ADTs
+    STACK = "stack"
+    QUEUE = "queue"
+    # ADT dictionary to validate
+    valid_adt = {
+        STACK: ["push", "pop", "top"],
+        QUEUE: ["enqueue", "dequeue", "front"]
+    }
+
+    def validate(method):
+        """
+        Decorator method to validate if called method is supported
+        for defined data structure.
+
+        Parameters
+        ----------
+        method : OBJECT [METHOD/FUNCTION]
+            Method object decorated to validate.
+
+        Returns
+        -------
+        allow : ANY
+            Returns response from decorated method.
+
+        """
+        # pylint: disable=no-member,no-self-argument,not-callable
+        def allow(self, *args, **kwargs):
+            if method.__name__ in \
+                    self.valid_adt.get(self.data_struct):
+                return method(self, *args, **kwargs)
+            print("ERROR: Method '%s' not allowed for data " \
+                  "structure: '%s'" % (method.__name__,
+                                       self.data_struct))
+            return None
+        return allow
 
     def __init__(self, size, ds_type="Stack"):
         """
@@ -29,20 +64,32 @@ class DataStructure():
         size : INTEGER
             Defines maximum size of data structure.
         ds_type : STRING, optional
-            Type of data structure to implement. The default is "Stack".
+            Type of data structure to implement. The default is
+            "Stack".
 
         Returns
         -------
         None.
 
         """
-        # Protected: define parameters
-        self._data_struct = ds_type.lower()
+        # General parameters
+        self.data_struct = ds_type.lower()
         self._size = size
-        self._top = -1
+        # Protected: define parameters
+        if self.data_struct == self.STACK:
+            self._top = -1
+        elif self.data_struct == self.QUEUE:
+            self._front = 0
+            self._rear = 0
+            # Add an extra buffer to address full/empty queue issue
+            self._size += 1
+        else:
+            raise Exception("ERROR: Not supported DS >",
+                            self.data_struct)
         # Private: List to store data [list based]
         self.__list = [None] * self._size
 
+    @validate
     def push(self, obj):
         """
         STACK: method to add/push data on to given stack[list].
@@ -64,6 +111,7 @@ class DataStructure():
             self._top += 1
             self.__list[self._top] = obj
 
+    @validate
     def pop(self):
         """
         STACK: method to remove/pop last element added from
@@ -85,20 +133,7 @@ class DataStructure():
             self._top -= 1
         return obj
 
-    def is_empty(self):
-        """
-        Method to check if defined data structure is empty
-
-        Returns
-        -------
-        bool
-            True if data structure is empty, False otherwise.
-
-        """
-        if self._top == -1:
-            return True
-        return False
-
+    @validate
     def top(self):
         """
         STACK: method to show top of the stack without removing it
@@ -111,6 +146,84 @@ class DataStructure():
         """
         return self.__list[self._top]
 
+    @validate
+    def enqueue(self, obj):
+        """
+        QUEUE: method to add/enqueue data to rear on given
+        queue[list].
+
+        Parameters
+        ----------
+        obj : ANY
+            Data object to enqueue.
+
+        Returns
+        -------
+        None.
+
+        """
+        if self.size() == self._size - 1:
+            # Error if queue is full
+            print("ERROR: Queue full")
+        else:
+            # Add object to rear of the queue
+            self.__list[self._rear] = obj
+            self._rear = (self._rear + 1) % self._size
+
+    @validate
+    def dequeue(self):
+        """
+        QUEUE: method to remove/dequeue data object from from of
+        given queue[list].
+
+        Returns
+        -------
+        obj : ANY
+            Data object dequeue/removed.
+
+        """
+        obj = None
+        if self.is_empty():
+            # Error if empty queue
+            print("ERROR: Queue empty")
+        else:
+            # Remove and return data object
+            obj = self.__list[self._front]
+            self.__list[self._front] = None
+            self._front = (self._front + 1) % self._size
+        return obj
+
+    @validate
+    def front(self):
+        """
+        QUEUE: method to show element in front of queue without
+        removing it.
+
+        Returns
+        -------
+        ANY
+            Front element on queue.
+
+        """
+        return self.__list[self._front]
+
+    def is_empty(self):
+        """
+        Method to check if defined data structure is empty
+
+        Returns
+        -------
+        bool
+            True if data structure is empty, False otherwise.
+
+        """
+        # Empty check based on defined data structure
+        if (self.data_struct == self.STACK and self._top == -1) or \
+                (self.data_struct == self.QUEUE and \
+                 self._front == self._rear):
+            return True
+        return False
+
     def size(self):
         """
         Method to return size of defined data structure.
@@ -121,17 +234,28 @@ class DataStructure():
             Size of populated data structure.
 
         """
-        return self._top + 1
+        ds_size = -1
+        if self.data_struct == self.STACK:
+            # Stack size calculation
+            ds_size = self._top + 1
+        elif self.data_struct == self.QUEUE:
+            # Size calculations for queue
+            ds_size = (self._size - self._front + self._rear) % self._size
+        return ds_size
 
 
 if __name__ == "__main__":
-    ds = DataStructure(1, "stack")
+    ds = DataStructure(3, "queue")
     print("EMPTY?:", ds.is_empty())
-    ds.push(1)
-    print("EMPTY?:", ds.is_empty())
-    ds.push(2)
-    print("TOP:", ds.top())
+    ds.enqueue(1)
     print("SIZE:", ds.size())
-    print("POP:", ds.pop())
-    print("EMPTY?:", ds.is_empty())
-    ds.pop()
+    ds.enqueue(2)
+    print("FRONT:", ds.front())
+    print("DEQUEUE:", ds.dequeue())
+    ds.enqueue(3)
+    ds.enqueue(4)
+    ds.enqueue(5)
+    print("DEQUEUE:", ds.dequeue())
+    ds.enqueue(5)
+    ds.enqueue(6)
+    print("FRONT:", ds.front())
